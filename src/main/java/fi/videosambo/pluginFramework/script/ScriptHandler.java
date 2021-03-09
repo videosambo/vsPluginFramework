b4814d0e5ab4fdfa4a57c3b31a4621c7386ae17c
videosambo <onnikontiokorpi@gmail.com>
2021-03-09 20:14:45 +0200

    Started to implement javax.script support for plugin backends

---
+++ src/main/java/fi/videosambo/pluginFramework/script/ScriptHandler.java
@@ -0,0 +1,12 @@
+package fi.videosambo.pluginFramework.script;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ScriptHandler {
+
+    private JavaPlugin plugin;
+
+    public ScriptHandler(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+}
